0251d92a3011c6e8ccf6dc570c2c6de1a8c57422
Alejandro Sanchez <160428383+zankYang@users.noreply.github.com>
2026-09-16 20:54:09 -0600

    Delete TypeScript directory

--- TypeScript/1_two_sums.ts
+++
@@ -1,9 +0,0 @@
-export function twoSum(nums: number[], target: number): number[] {
-    const map = new Map<number, number>();
-    for (let i = 0; i < nums.length; i++) {
-      const need = target - nums[i];
-      if (map.has(need)) return [map.get(need)!, i];
-      map.set(nums[i], i);
-    }
-    return [];
-  }
